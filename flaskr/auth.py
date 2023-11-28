3d79a48eb3416cb635a44834d77bfd54183dd671
wei <xwei0012@student.monash.edu>
2023-11-28 16:00:56 +0800

    Finish with flask_sqlalchemy

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -5,8 +5,10 @@ from flask import (
 )
 
 from werkzeug.security import check_password_hash, generate_password_hash
-
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
 from flaskr.db import get_db
+from .models import User
 
 bp = Blueprint('auth',__name__,url_prefix='/auth')
 
@@ -25,12 +27,13 @@ def register():
 
         if error is None:
             try:
-                db.execute(
-                    'INSERT INTO user (username, password) VALUES (?, ?)',
-                    (username, generate_password_hash(password)),
+                user = User(
+                    username = username,
+                    password = generate_password_hash(password)
                 )
-                db.commit()
-            except db.IntegrityError:
+                db.session.add(user)
+                db.session.commit()
+            except IntegrityError:
                 error = f"User {username} is already registered."
             else:
                 return redirect(url_for("auth.login"))
@@ -44,18 +47,16 @@ def login():
         password = request.form['password']
         db = get_db()
         error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
 
+        user = db.session.query(User).filter_by(username=username).first()
         if user is None:
             error = 'Incorrect username'
-        elif not check_password_hash(user['password'],password):
+        elif not check_password_hash(user.password,password):
             error = 'Incorrect password'
-        
+
         if error is None:
             session.clear()
-            session['user_id'] = user['id']
+            session['user_id'] = user.id
             return redirect(url_for('index'))
         flash(error)
     return render_template('auth/login.html')
@@ -66,9 +67,7 @@ def load_logged_in_user():
     if user_id is None:
         g.user = None
     else:
-        g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?', (user_id,)
-        ).fetchone()
+        g.user = get_db().session.query(User).filter_by(id=user_id).first()
 
 @bp.route('/logout',methods=['GET','POST'])
 def logout():
--- flaskr/models/post.py
+++ flaskr/models/post.py
@@ -7,12 +7,12 @@ class Post(db.Model):
     __tablename__ = "post"
 
     id = db.Column(db.Integer,primary_key=True)
-    author_id = db.Column(db.Integer,unique=False,nullable=False)
+    # author_id = db.Column(db.Integer,unique=False,nullable=False)
     created = db.Column(db.DateTime, default=datetime.utcnow)
     title = db.Column(db.String(50),nullable=False)
     body = db.Column(db.Text, nullable=False)
 
-    user_id = db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+    author_id = db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
 
     def __repr__(self):
         return f'<Post {self.title}>'
