055e3c3b6b82ce4df4a2587b9af32e878a2da916
wei <xwei0012@student.monash.edu>
2023-11-28 11:55:44 +0800

    Add SQLAlchemy and it's initialization

---
+++ flaskr/models/user.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flaskr.db import get_db
+
+db = get_db()
+class User(db.Model):
+    
+    __tablename__ = "user"
+
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(50),unique=True,nullable=False)
+    password = db.Column(db.String(128),unique=False,nullable=False)
+
+    posts = db.relationship('Post',backref='author',lazy=True)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
