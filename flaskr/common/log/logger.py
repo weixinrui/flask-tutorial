c18de7110588f25eeeea38af088e5c48481d5e43
wei <weixinruiyuanban@gmail.com>
2023-11-30 17:26:33 +0800

    Fix some code

---
+++ flaskr/common/log/logger.py
@@ -0,0 +1,52 @@
+import os
+import threading
+from logging import FileHandler, StreamHandler
+
+def get_filepath(filename):
+    
+    log_path = os.path.join(os.path.split(os.path.realpath(__file__))[0],'../../logs')
+
+    if not os.path.exists(log_path):
+        os.mkdir(log_path)
+    
+    if filename is None:
+        filepath = os.path.join(log_path,"flaskr.log")
+    else:
+        filepath = os.path.join(log_path,filename)
+    return filepath
+
+class RunLogger:
+    __instance_lock = threading.Lock()
+
+    def __init__(self,level,formatter,filename=None):
+        self.level = level
+        self.log_formatter = formatter
+        self.filename = filename
+        self.filepath = get_filepath(filename)
+        self.logger = self.__get_logger()
+
+    def __new__(cls,*args,**kwargs):
+        if not hasattr(RunLogger,'_instance'):
+            with RunLogger.__instance_lock:
+                if not hasattr(RunLogger,'_instance'):
+                    RunLogger._instance = object.__new__(cls)
+        return RunLogger._instance
+    
+    def get_file_handle(self):
+        handle = FileHandler(self.filepath,encoding='utf-8')
+        handle.setFormatter(self.log_formatter)
+        return handle
+
+    def get_stream_handle(self):
+        handle = StreamHandler(filename=self.filepath,encoding='utf-8')
+        handle.setFormatter(self.log_formatter)
+        return handle
+    
+    def __get_logger(self):
+        if self.filename is None:
+            logger = logging.getLogger("run.log")
+        else:
+            logger = logging.getLogger(self.filename)
+        logger.setLevel(self.level)
+        return logger
+    
--- flaskr/common/log_formatter/request_formatter.py
+++ flaskr/common/log_formatter/request_formatter.py
@@ -1,52 +1,21 @@
-import os
-import threading
-from logging import FileHandler, StreamHandler
+import logging
+from flask import has_request_context, request, session
 
-def get_filepath(filename):
+class RequestFormatter(logging.Formatter):
     
-    log_path = os.path.join(os.path.split(os.path.realpath(__file__))[0],'../../logs')
-
-    if not os.path.exists(log_path):
-        os.mkdir(log_path)
-    
-    if filename is None:
-        filepath = os.path.join(log_path,"flaskr.log")
-    else:
-        filepath = os.path.join(log_path,filename)
-    return filepath
-
-class RunLogger:
-    __instance_lock = threading.Lock()
-
-    def __init__(self,level,formatter,filename=None):
-        self.level = level
-        self.log_formatter = formatter
-        self.filename = filename
-        self.filepath = get_filepath(filename)
-        self.logger = self.__get_logger()
-
-    def __new__(cls,*args,**kwargs):
-        if not hasattr(RunLogger,'_instance'):
-            with RunLogger.__instance_lock:
-                if not hasattr(RunLogger,'_instance'):
-                    RunLogger._instance = object.__new__(cls)
-        return RunLogger._instance
-    
-    def get_file_handle(self):
-        handle = FileHandler(self.filepath,encoding='utf-8')
-        handle.setFormatter(self.log_formatter)
-        return handle
-
-    def get_stream_handle(self):
-        handle = StreamHandler(filename=self.filepath,encoding='utf-8')
-        handle.setFormatter(self.log_formatter)
-        return handle
-    
-    def __get_logger(self):
-        if self.filename is None:
-            logger = logging.getLogger("run.log")
+    def format(self, record):
+        if has_request_content(record):
+            record.url = request.url
+            record.remote_addr = request.remote_addr
+            record.method = request.method
+            record.form = dict(request.form)
+            record.files = dict(request.files)
+            record.json = dict(request.json)
         else:
-            logger = logging.getLogger(self.filename)
-        logger.setLevel(self.level)
-        return logger
-    
+            record.url = None
+            record.remote_addr = None
+            record.method = None
+            record.form = None
+            record.files = None
+            record.json = None
+        return super(RequestFormatter, self).format(record)
